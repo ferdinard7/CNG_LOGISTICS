bbebb5f0d74ccee050e87d4b6b92e23c9bf3ef67
Big_Ferd <tobeferdinard@gmail.com>
2026-01-05 20:56:53 +0100

    fixed the auth error

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -7,7 +7,7 @@ const prisma = new PrismaClient();
 export const connectPrisma = async() => {
   try {
     await prisma.$connect();
-    logger.info('✅ Prisma connected to MySQL database successfully');
+    logger.info('✅ Prisma connected to postgreSQL database successfully');
   } catch (err) {
     logger.error('❌ Prisma failed to connect to database:', err);
     process.exit(1);
